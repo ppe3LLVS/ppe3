83161b61f34a2db32cd1b0cab098fdd3c064aa15
ValocheBG <74363714+ValocheBG@users.noreply.github.com>
2020-12-17 11:34:02 +0100

    Add files via upload

---
+++ adresses.sql
@@ -0,0 +1,44 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3307
+-- Généré le :  jeu. 17 déc. 2020 à 10:29
+-- Version du serveur :  10.3.14-MariaDB
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ppe3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresses`
+--
+
+DROP TABLE IF EXISTS `adresses`;
+CREATE TABLE IF NOT EXISTS `adresses` (
+  `id_adr` int(5) NOT NULL AUTO_INCREMENT,
+  `numero_adr` int(5) NOT NULL,
+  `lieu_adr` varchar(50) NOT NULL,
+  `cp_adr` int(5) NOT NULL,
+  `ville_adr` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_adr`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
